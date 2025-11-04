2d3b62ff075efaf6bd1e37645b2adbd3ad3c0593
sniff <sniff@sniff.cc>
2025-11-03 19:23:12 -0800

    springe

---
+++ src/main/java/ca/kpu/info2413/library/backend/LibraryApplicationSpring.java
@@ -0,0 +1,13 @@
+package ca.kpu.info2413.library.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplicationSpring
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LibraryApplicationSpring.class, args);
+    }
+}
