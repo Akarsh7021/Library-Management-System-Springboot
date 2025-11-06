d087975b9fb2b2d78549947d8f0aac962881e1aa
chloe <chloechangling@gmail.com>
2025-11-06 13:01:34 -0800

    Publication

--- src/main/java/ca/kpu/info2413/library/backend/Publication.java
+++ src/main/java/ca/kpu/info2413/library/backend/Publication.java
@@ -1,5 +1,26 @@
 package ca.kpu.info2413.library.backend;
 
-public class Publication
-{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Publication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int isbn_13;
+    private String title;
+    private java.sql.Date publication_date;
+    private int page_count;
+    private String genre;
+
+    // for JPA only, no use
+    public Publication() {
+    }
+
+    // getters, setters and constructor
 }
