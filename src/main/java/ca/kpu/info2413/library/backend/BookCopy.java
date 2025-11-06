6430a47478f103790a6e2dd8db5eeffdf2a9c4c6
chloe <chloechangling@gmail.com>
2025-11-06 00:46:57 -0800

    book copy

--- src/main/java/ca/kpu/info2413/library/backend/BookCopy.java
+++ src/main/java/ca/kpu/info2413/library/backend/BookCopy.java
@@ -1,5 +1,26 @@
 package ca.kpu.info2413.library.backend;
 
-public class BookCopy
-{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class BookCopy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private LocalDate publishDate;
+
+    // for JPA only, no use
+    public BookCopy() {
+    }
+
+    // getters, setters and constructor
 }
+
