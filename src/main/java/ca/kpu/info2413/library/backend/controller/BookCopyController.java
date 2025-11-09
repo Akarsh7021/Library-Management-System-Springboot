efed8763414a3d94935af16feb04ffa81fe875a1
sniff <sniff@sniff.cc>
2025-11-09 15:41:26 -0800

    end point book copy

---
+++ src/main/java/ca/kpu/info2413/library/backend/controller/BookCopyController.java
@@ -0,0 +1,57 @@
+package ca.kpu.info2413.library.backend.controller;
+
+import ca.kpu.info2413.library.backend.model.BookCopy;
+import ca.kpu.info2413.library.backend.service.BookCopyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/book_copy")
+public class BookCopyController
+{
+
+    @Autowired
+    private BookCopyService bookCopyService;
+
+    @GetMapping
+    public List<BookCopy> findAll()
+    {
+        return bookCopyService.findAll();
+    }
+
+    @GetMapping("/{serial_barcode}")
+    public Optional<BookCopy> findBySerialBarcode(@PathVariable Integer serial_barcode)
+    {
+        return bookCopyService.findBySerialBarcode(serial_barcode);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public BookCopy create(@RequestBody BookCopy bookCopy)
+    {
+        return bookCopyService.save(bookCopy);
+    }
+
+    @PutMapping
+    public BookCopy update(@RequestBody BookCopy bookCopy)
+    {
+        return bookCopyService.save(bookCopy);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{serial_barcode}")
+    public void deleteBySerialBarcode(@PathVariable Integer serial_barcode)
+    {
+        bookCopyService.deleteBySerialBarcode(serial_barcode);
+    }
+
+    @GetMapping("/find/isbn/{isbn}")
+    public List<BookCopy> findByIsbn13(@PathVariable Integer isbn)
+    {
+        return bookCopyService.findByIsbn13(isbn);
+    }
+}
---
+++ src/main/java/ca/kpu/info2413/library/backend/service/BookCopyService.java
@@ -0,0 +1,50 @@
+package ca.kpu.info2413.library.backend.service;
+
+import ca.kpu.info2413.library.backend.model.BookCopy;
+import ca.kpu.info2413.library.backend.model.Publication;
+import ca.kpu.info2413.library.backend.repository.BookCopyRepository;
+import ca.kpu.info2413.library.backend.repository.PublicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookCopyService
+{
+    @Autowired
+    BookCopyRepository bookCopyRepository;
+    @Autowired
+    PublicationRepository publicationRepository;
+
+    public List<BookCopy> findAll()
+    {
+        return bookCopyRepository.findAll();
+    }
+
+    public Optional<BookCopy> findBySerialBarcode(Integer serialBarcode)
+    {
+        return bookCopyRepository.findBySerialBarcode(serialBarcode);
+    }
+
+    public BookCopy save(BookCopy bookCopy)
+    {
+        return bookCopyRepository.save(bookCopy);
+    }
+
+    public void deleteBySerialBarcode(Integer serialBarcode)
+    {
+        bookCopyRepository.deleteById(serialBarcode);
+    }
+
+    public List<BookCopy> findByPublication(Publication publication)
+    {
+        return bookCopyRepository.findByPublication(publication);
+    }
+
+    public List<BookCopy> findByIsbn13(Integer isbn13)
+    {
+        return findByPublication(publicationRepository.findByIsbn13(isbn13).orElse(null));
+    }
+}
