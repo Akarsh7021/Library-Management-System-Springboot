59bc0b0d9f78925e183e6f3ea46b6962dc2ecd10
sniff <sniff@sniff.cc>
2025-11-09 15:45:17 -0800

    oops lmbok

--- src/main/java/ca/kpu/info2413/library/backend/model/PublicationPublicationId.java
+++ src/main/java/ca/kpu/info2413/library/backend/model/PublicationPublicationId.java
@@ -1,9 +1,19 @@
 package ca.kpu.info2413.library.backend.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 public final class PublicationPublicationId implements Serializable
 {
     private static final long serialVersionUID = 1L;
