e19e55d4319855e76beb1fa0000e21ef628976df
sniff <sniff@sniff.cc>
2025-11-09 00:28:46 -0800

    publicationauthor

--- src/main/java/ca/kpu/info2413/library/backend/PublicationAuthor.java
+++ src/main/java/ca/kpu/info2413/library/backend/PublicationAuthor.java
@@ -1,24 +1,52 @@
 package ca.kpu.info2413.library.backend;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import lombok.Data;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Data
-@NoArgsConstructor
-public class PublicationAuthor
-{
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "author_id_Author")
-    private Integer authorIdAuthor;
+@Table(name = "PublicationAuthor")
+@Getter
+@Setter
+@ToString(exclude = {"author", "publication"}) // avoid lazy-loading / recursion in toString
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA requires a no-arg ctor (protected is fine)
+@AllArgsConstructor
+@Builder
+public class PublicationAuthor {
 
-    @Column(name = "isbn_13_Publication")
-    private Integer isbn13Publication;
+    @EmbeddedId
+    private PublicationAuthorId id;
+
+    @MapsId("authorIdAuthor")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id_Author", nullable = false)
+    private Author author;
+
+    @MapsId("isbn13Publication")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "isbn_13_Publication", nullable = false)
+    private Publication publication;
+
+    /**
+     * Convenience ctor. Adjust the getter names below if your Author/Publication use different method names.
+     */
+    public PublicationAuthor(Author author, Publication publication) {
+        this.author = author;
+        this.publication = publication;
+        this.id = new PublicationAuthorId(
+                author != null ? author.getAuthorId() : null,           // adjust if your getter is getId()
+                publication != null ? publication.getIsbn13() : null // adjust if different
+        );
+    }
 }
