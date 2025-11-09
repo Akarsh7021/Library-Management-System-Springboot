d7ba802dd4da24f6d258ff441f3dadcebb120010
sniff <sniff@sniff.cc>
2025-11-09 14:23:03 -0800

    wait… list?

---
+++ src/main/java/ca/kpu/info2413/library/backend/AccountPublicationId.java
@@ -0,0 +1,26 @@
+package ca.kpu.info2413.library.backend;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class AccountPublicationId implements Serializable
+{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "account_id_Account", nullable = false)
+    private Integer accountIdAccount;
+
+    @Column(name = "isbn_13_Publication", nullable = false)
+    private Integer isbn13Publication;
+}
