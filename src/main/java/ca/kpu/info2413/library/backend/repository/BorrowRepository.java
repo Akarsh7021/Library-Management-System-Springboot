4533c52df43c42badaff44858e6c8c41148ea4c5
chloe <chloe.chang@student.kpu.ca>
2025-11-27 17:11:32 -0800

    feat: scheduler to create new fines and recalculate existing fines.

--- src/main/java/ca/kpu/info2413/library/backend/repository/BorrowRepository.java
+++ src/main/java/ca/kpu/info2413/library/backend/repository/BorrowRepository.java
@@ -3,6 +3,7 @@ package ca.kpu.info2413.library.backend.repository;
 import ca.kpu.info2413.library.backend.model.Borrow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 // Spring Data JPA creates CRUD implementation at runtime automatically.
@@ -16,4 +17,6 @@ public interface BorrowRepository extends JpaRepository<Borrow, Integer>
     List<Borrow> findBySerialBarcodeBookCopy(Integer serialBarcodeBookCopy);
 
     List<Borrow> findByAccountIdAccount(Integer accountIdAccount);
+
+    List<Borrow> findByDueDateBeforeAndStatus(LocalDate dueDate, String status);
 }
